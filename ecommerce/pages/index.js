4cb6fddc352c12ff91defeec7bc4bd398acfc182
Rose <rosemwangi305@gmail.com>
2022-05-14 18:06:23 +0300

    styling

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+
       <>
       HeroBanner
 
@@ -15,9 +15,14 @@ const Home = () => {
         </p>
       </div>
 
+      <div>
+        {['product 1','product 2'].map(
+          (product) => product)}
+      </div>
+      Footer
       </>
 
-    </div>
+
   )
 }
 
